bac89ae86896fca17d671834cde37cdc96840c08
EmiGutierrez16 <emilio.gutierrez1891@gmail.com>
2025-02-13 12:05:04 -0300

    se ajusta el estilo del carrito en la barra de navegacion

--- Proyecto_Final/src/componentes/BarraNavegacion.jsx
+++ Proyecto_Final/src/componentes/BarraNavegacion.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Logo from "../assets/imagenes/logo.svg";
 import Perfil from "../assets/imagenes/image-avatar.png";
 import CartIcon from "../assets/imagenes/icons/icon-cart.svg";
@@ -10,19 +10,43 @@ import Carrito from "./Carrito";
 const BarraNavegacion = ({ productos }) => {
   const [toggleCarrito, setToggleCarrito] = React.useState(false);
   const [open, setOpen] = React.useState(false);
+  const carritoRef = useRef(null);
+
+  // Función para cerrar el carrito si se hace clic fuera
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (carritoRef.current && !carritoRef.current.contains(event.target)) {
+        setToggleCarrito(false);
+      }
+    }
+
+    if (toggleCarrito) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [toggleCarrito]);
 
   return (
     <nav className="barra-navegacion">
       <div className="logo">
         <ul>
-          {!open && <li onClick={() => setOpen(true)}>
-            <img src={MenuIcon} alt="Menu-icon" />
-          </li>}
-          {open && <li onClick={() => setOpen(false)}>
-            <img src={Close} alt="Close-icon" />
-          </li>}
+          {!open && (
+            <li onClick={() => setOpen(true)}>
+              <img src={MenuIcon} alt="Menu-icon" />
+            </li>
+          )}
+          {open && (
+            <li onClick={() => setOpen(false)}>
+              <img src={Close} alt="Close-icon" />
+            </li>
+          )}
           <li>
-          <img src={Logo} alt="Logo" />
+            <img src={Logo} alt="Logo" />
           </li>
         </ul>
       </div>
@@ -33,19 +57,18 @@ const BarraNavegacion = ({ productos }) => {
         <li>Acerca de</li>
         <li>Contacto</li>
       </ul>
-      <div className="iconos">
-        <span className="carrito">
-          <img
-            src={CartIcon}
-            alt="Carrito Icon"
-            onClick={() => setToggleCarrito(!toggleCarrito)}
-          />
-          {toggleCarrito && <Carrito productos={productos}></Carrito>}
-        </span>
-        <span className="perfil">
-          <img src={Perfil} alt="Perfil" />
-        </span>
-      </div>
+      <div className="carrito">
+  <img
+    src={CartIcon}
+    alt="Carrito Icon"
+    onClick={() => setToggleCarrito(!toggleCarrito)}
+  />
+  {toggleCarrito && (
+    <div className="carrito-contenedor">
+      <Carrito productos={productos} />
+    </div>
+  )}
+</div>
     </nav>
   );
 };
